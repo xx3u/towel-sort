a2be0ac089a2f894d8d47d6432828faccdd714fd
YK <yuliya.khv@gmail.com>
2021-02-16 15:28:07 +0600

    feat: added sort function for every 2nd element in array and then flat function for matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == 0 || matrix === undefined) {
+    return [];
+  } else {
+    for (let i = 0; i < matrix.length; i ++) {
+      if (i % 2) {
+        matrix[i].sort((a, b) => b - a)
+      } else {
+        matrix[i]
+      } 
+    }  
+    return matrix.flat()
+  };
 }
--- test.js
+++ test.js
@@ -5,11 +5,11 @@ const towelSort = require('./src/index.js');
 describe('.towelSort, should perfrom \'towelSort\'', () => {
 
   it('Should return empty array if no params passed', () => {
-    assert.deepEqual(towelSort(), []);
+    assert.deepStrictEqual(towelSort(), []);
   });
 
   it('Should return empty array if matrix is empty', () => {
-    assert.deepEqual(towelSort([]), []);
+    assert.deepStrictEqual(towelSort([]), []);
   });
 
   it('should do towelSort', () => {
@@ -18,7 +18,7 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
       [3, 4],
     ];
 
-    assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 3 ]);
+    assert.deepStrictEqual(towelSort(matrix), [ 1, 2, 4, 3 ]);
   });
 
   it('should do towelSort', () => {
@@ -28,7 +28,7 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
       [7, 8, 9],
     ];
 
-    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]);
+    assert.deepStrictEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]);
   });
 
   it('should do towelSort', () => {
@@ -39,7 +39,7 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
         [13, 14, 15, 16],
     ];
 
-    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]);
+    assert.deepStrictEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]);
   });
 
   it('should do towelSort correctrly with nonsquare matrix', () => {
@@ -49,7 +49,7 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
         [9, 10, 11, 12],
     ];
 
-    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12,]);
+    assert.deepStrictEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12,]);
   });
 
   it('should do towelSort', () => {
@@ -58,7 +58,7 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
       [4, 5, 6],
     ];
 
-    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, ]);
+    assert.deepStrictEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, ]);
   });
 
   it('should do towelSort', () => {
@@ -68,7 +68,7 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
         [9, 12],
     ];
 
-    assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 8, 7, 6, 5, 9, 12 ]);
+    assert.deepStrictEqual(towelSort(matrix), [ 1, 2, 4, 8, 7, 6, 5, 9, 12 ]);
   });
 
 });
